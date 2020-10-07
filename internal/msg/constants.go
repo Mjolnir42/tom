64a3aa010e6539c681a1ff7906b1ee92c82157fd
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2020-10-07 16:20:59 +0000

    msg: add RFC3339Milli constant

--- internal/msg/constants.go
+++ internal/msg/constants.go
@@ -14,4 +14,9 @@ const (
 	ActionShow    = `show`
 )
 
+const (
+	// RFC3339Milli is a format string for millisecond precision RFC3339
+	RFC3339Milli string = `2006-01-02T15:04:05.000Z07:00`
+)
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
