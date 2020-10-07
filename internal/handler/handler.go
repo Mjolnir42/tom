ef73cf60e6abc4bcf5bdd5eef257fb5fe0ffe956
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2020-10-07 15:48:58 +0000

    handler: request handler registration module
    handler is the internal module used by the application handlers
    to register themselves with.
    This allows the rest module to lookup where to route requests.

---
+++ internal/handler/handler.go
@@ -0,0 +1,27 @@
+/*-
+ * Copyright (c) 2020, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package handler // import "github.com/mjolnir42/soma/internal/handler"
+
+import (
+	"database/sql"
+
+	"github.com/mjolnir42/tom/internal/msg"
+	"github.com/sirupsen/logrus"
+)
+
+// Handler process a specific request type
+type Handler interface {
+	Register(*sql.DB, ...*logrus.Logger)
+	Run()
+	ShutdownNow()
+	Intake() chan msg.Request
+	PriorityIntake() chan msg.Request
+	RegisterRequests(*Map)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
