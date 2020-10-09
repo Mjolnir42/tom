fd2866825499b1b26ec22763949ff3d773b53e2a
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2020-10-09 13:14:54 +0200

    proto: update string methods for server

--- pkg/proto/server.go
+++ pkg/proto/server.go
@@ -19,16 +19,15 @@ type Server struct {
 }
 
 func (s *Server) String() string {
-	str := `tom://` + s.Namespace + `/server`
-	switch {
-	case s.Name != `` && s.ID != ``:
-		str = str + `/name=` + s.Name + `,id=` + s.ID
-	case s.Name != ``:
-		str = str + `/name=` + s.Name
-	case s.ID != ``:
-		str = str + `/id=` + s.ID
-	}
-	return str
+	return s.DNS()
+}
+
+func (s *Server) DNS() string {
+	return s.Name + `.` + s.Namespace + `.` + `server.tom`
+}
+
+func (s *Server) TomID() string {
+	return `tom://` + s.Namespace + `/server` + `/name=` + s.Name
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
