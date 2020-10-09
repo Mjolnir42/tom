73ed43e20c433fdb0db7a5ff2ec0e95a1ea0fb7d
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2020-10-09 13:15:53 +0200

    proto: add runtime

---
+++ pkg/proto/runtime.go
@@ -0,0 +1,33 @@
+/*-
+ * Copyright (c) 2020, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package proto //
+
+// Runtime defines a runtime within the asset model
+type Runtime struct {
+	ID        string     `json:"id"`
+	Namespace string     `json:"namespace"`
+	Name      string     `json:"name"`
+	Type      string     `json:"type"`
+	Parent    string     `json:"parent"`
+	Link      []string   `json:"link"`
+	Property  []Property `json:"property"`
+}
+
+func (r *Runtime) String() string {
+	return r.DNS()
+}
+
+func (r *Runtime) DNS() string {
+	return r.Name + `.` + r.Namespace + `.` + `runtime.tom`
+}
+
+func (r *Runtime) TomID() string {
+	return `tom://` + r.Namespace + `/runtime` + `/name=` + r.Name
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
