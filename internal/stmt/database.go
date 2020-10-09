7084aabf9354075917fe50c808fcc38213822947
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2020-10-09 11:39:25 +0200

    stmt: add database configuration statements

---
+++ internal/stmt/database.go
@@ -0,0 +1,26 @@
+/*-
+ * Copyright (c) 2020, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package stmt // import "github.com/mjolnir42/tom/internal/stmt"
+
+const (
+	DatabaseStatements = ``
+
+	DatabaseTimezone = `SET TIME ZONE 'UTC';`
+
+	DatabaseIsolationLevel = `SET SESSION CHARACTERISTICS AS TRANSACTION ISOLATION LEVEL SERIALIZABLE;`
+
+	ReadOnlyTransaction = `SET TRANSACTION READ ONLY, DEFERRABLE;`
+)
+
+func init() {
+	m[DatabaseIsolationLevel] = `DatabaseIsolationLevel`
+	m[DatabaseTimezone] = `DatabaseTimezone`
+	m[ReadOnlyTransaction] = `ReadOnlyTransaction`
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
