317ff1ae0514a4e0c46cd054c9aa4c2f7e16655f
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2020-10-22 15:42:53 +0200

    proto: add interface Entity

---
+++ pkg/proto/entity.go
@@ -0,0 +1,17 @@
+/*-
+ * Copyright (c) 2020, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package proto //
+
+type Entity interface {
+	String() string
+	FormatDNS() string
+	FormatTomID() string
+	ParseTomID() error
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
