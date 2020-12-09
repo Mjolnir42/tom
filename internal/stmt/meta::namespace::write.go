24e71827ea48777a2ce943f75f2b1103c6ef898e
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2020-12-09 17:15:50 +0000

    stmt: add NamespaceAdd query

--- internal/stmt/meta::namespace::write.go
+++ internal/stmt/meta::namespace::write.go
@@ -11,7 +11,28 @@ const (
 	NamespaceWriteStatements = ``
 
 	NamespaceAdd = `
-SELECT      'Namespace.ADD';`
+WITH ins_dct AS ( INSERT INTO meta.dictionary ( name )
+                  VALUES      ( $1::text )
+                  ON CONFLICT ( name ) DO UPDATE SET name=EXCLUDED.name
+                  RETURNING   dictionaryID AS dictID, name AS dictName),
+     ins_reg AS ( INSERT INTO meta.attribute ( dictionaryID, attribute )
+                  SELECT      dictID,
+                              'dict_name'
+                  FROM        cte ),
+     ins_nam AS ( INSERT INTO meta.unique_attribute ( dictionaryID, attribute )
+                  SELECT      dictID,
+                              'dict_name'
+                  FROM        ins_dct
+                  ON CONFLICT ON CONSTRAINT __uniq_unique_attr DO UPDATE SET dictionaryID=EXCLUDED.dictionaryID
+                  RETURNING   attributeID AS attrID )
+INSERT INTO       meta.dictionary_unique_attribute_values ( dictionaryID, attributeID, value, validity )
+SELECT            dictID,
+                  attrID,
+                  dictName,
+                  '[-infinity,infinity]'::tstzrange
+FROM              ins_dct
+  CROSS JOIN      ins_nam
+ON CONFLICT       ON CONSTRAINT __mdq_temporal DO NOTHING;`
 
 	NamespaceRemove = `
 SELECT      'Namespace.REMOVE';`
