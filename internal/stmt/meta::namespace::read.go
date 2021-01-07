13d8c799e60de3b0c940367f7a499551f2cc2f7f
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2021-01-07 14:09:52 +0000

    stmt: add NamespaceList SQL statement

--- internal/stmt/meta::namespace::read.go
+++ internal/stmt/meta::namespace::read.go
@@ -11,37 +11,12 @@ const (
 	NamespaceReadStatements = ``
 
 	NamespaceList = `
-SELECT      asset.server.serverID,
-            meta.dictionary.name,
-            meta.standard_attribute.attribute,
-            asset.server_standard_attribute_values.value
-FROM        asset.server
-    JOIN    meta.dictionary
-      ON    asset.server.dictionaryID = meta.dictionary.dictionaryID
-    JOIN    meta.standard_attribute
-      ON    meta.dictionary.dictionaryID = meta.standard_attribute.dictionaryID
-    JOIN    asset.server_standard_attribute_values
-      ON    asset.server_standard_attribute_values.serverID = asset.server.serverID
-     AND    asset.server_standard_attribute_values.dictionaryID = asset.server.dictionaryID
-     AND    asset.server_standard_attribute_values.attributeID = meta.standard_attribute.attributeID
-   WHERE    now()::timestamptz(3) <@ asset.server_standard_attribute_values.validity
-     AND    meta.standard_attribute.attribute IN ('type')
-UNION
-SELECT      asset.server.serverID,
-            meta.dictionary.name,
-            meta.unique_attribute.attribute,
-            asset.server_unique_attribute_values.value
-FROM        asset.server
-    JOIN    meta.dictionary
-      ON    asset.server.dictionaryID = meta.dictionary.dictionaryID
-    JOIN    meta.unique_attribute
-      ON    meta.dictionary.dictionaryID = meta.unique_attribute.dictionaryID
-    JOIN    asset.server_unique_attribute_values
-      ON    asset.server_unique_attribute_values.serverID = asset.server.serverID
-     AND    asset.server_unique_attribute_values.dictionaryID = asset.server.dictionaryID
-     AND    asset.server_unique_attribute_values.attributeID = meta.unique_attribute.attributeID
-WHERE       now()::timestamptz(3) <@ asset.server_unique_attribute_values.validity
-     AND    meta.unique_attribute.attribute IN ('name');`
+SELECT      meta.dictionary.name,
+            meta.dictionary.createdAt,
+            inventory.user.uid
+FROM        meta.dictionary
+    JOIN    inventory.user
+      ON    meta.dictionary.createdBy = inventory.user.userID;`
 
 	NamespaceShow = `
 SELECT      'Namespace.SHOW';`
