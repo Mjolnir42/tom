7c21a427406f7fe9261eb8176b4c37cbf71b388e
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2021-07-11 18:19:07 +0000

    cli: add internal library with cli helper

---
+++ internal/cli/cmpl/generic.go
@@ -0,0 +1,46 @@
+package cmpl
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+)
+
+func In(c *cli.Context) {
+	Generic(c, []string{`in`})
+}
+
+func Generic(c *cli.Context, keywords []string) {
+	switch {
+	case c.NArg() == 0:
+		return
+	case c.NArg() == 1:
+		for _, t := range keywords {
+			fmt.Println(t)
+		}
+		return
+	}
+
+	skip := 0
+	match := make(map[string]bool)
+
+	for _, t := range c.Args().Tail() {
+		if skip > 0 {
+			skip--
+			continue
+		}
+		skip = 1
+		match[t] = true
+		continue
+	}
+	// do not complete in positions where arguments are expected
+	if skip > 0 {
+		return
+	}
+	for _, t := range keywords {
+		if !match[t] {
+			fmt.Println(t)
+		}
+	}
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
