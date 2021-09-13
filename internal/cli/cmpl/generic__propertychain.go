c995cd3b8cb90dc0f21f30b5fb5d2a1f7e3e66ab
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2021-09-13 15:17:56 +0200

    cmpl: add parsing for commands with property arguments

--- internal/cli/cmpl/generic__propertychain.go
+++ internal/cli/cmpl/generic__propertychain.go
@@ -16,6 +16,49 @@ import (
 	"github.com/urfave/cli/v2"
 )
 
+func GenericMultiWithProperty(c *cli.Context, singlewords, multiwords []string) {
+	keywords := append(singlewords, multiwords...)
+
+	switch {
+	case c.NArg() == 0:
+		return
+	case c.NArg() == 1:
+		for _, t := range keywords {
+			fmt.Println(t)
+		}
+		return
+	}
+
+	skip := 0
+	match := make(map[string]bool)
+
+	for i, t := range c.Args().Tail() {
+		if skip > 0 {
+			skip--
+			continue
+		}
+		skip = 1
+		match[t] = true
+		if t == `property` {
+			propertySubChain(c.Args().Tail()[i:], singlewords, multiwords)
+			return
+		}
+		continue
+	}
+	// do not complete in positions where arguments are expected
+	if skip > 0 {
+		return
+	}
+	for _, t := range singlewords {
+		if !match[t] {
+			fmt.Println(t)
+		}
+	}
+	for _, t := range multiwords {
+		fmt.Println(t)
+	}
+}
+
 // GenericPropertyChain tab-completes with property chains
 func GenericPropertyChain(c *cli.Context, singlewords, multiwords []string) {
 	keywords := append(singlewords, multiwords...)
@@ -38,6 +81,66 @@ func GenericPropertyChain(c *cli.Context, singlewords, multiwords []string) {
 	chainMatch := make(map[string]bool)
 
 	for _, t := range c.Args().Tail() {
+		if skip > 0 {
+			skip--
+			continue
+		}
+		skip = 1
+		match[t] = true
+		if t == `property` {
+			inChain = true
+			chainMatch = map[string]bool{}
+			chainMatch[t] = true
+		} else if inChain {
+			switch t {
+			case `value`:
+				chainMatch[t] = true
+			case `since`:
+				chainMatch[t] = true
+			case `until`:
+				chainMatch[t] = true
+			default:
+				inChain = false
+			}
+		}
+		continue
+	}
+	// do not complete in positions where arguments are expected
+	if skip > 0 {
+		return
+	}
+	// within the property chain, only complete chain arguments
+	if inChain {
+		for _, t := range chainwords {
+			if !chainMatch[t] {
+				fmt.Println(t)
+			}
+		}
+		if chainMatch[`value`] {
+			fmt.Println(`property`)
+		}
+		return
+	}
+	for _, t := range singlewords {
+		if !match[t] {
+			fmt.Println(t)
+		}
+	}
+	for _, t := range multiwords {
+		fmt.Println(t)
+	}
+}
+
+func propertySubChain(args []string, singlewords, multiwords []string) {
+	chainwords := []string{`value`, `since`, `until`}
+
+	inChain := false
+
+	skip := 0
+	match := make(map[string]bool)
+	chainMatch := make(map[string]bool)
+
+	for _, t := range args {
 		if skip > 0 {
 			skip--
 			continue
