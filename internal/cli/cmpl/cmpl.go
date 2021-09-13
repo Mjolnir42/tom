48cb2f26feae386c926000131b804abef5829cff
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2021-09-13 15:12:28 +0200

    cmpl: add DirectMulti to support completion with no object name

--- internal/cli/cmpl/cmpl.go
+++ internal/cli/cmpl/cmpl.go
@@ -96,4 +96,41 @@ func GenericMultiTriple(c *cli.Context, singlewords, multiwords []string) {
 	}
 }
 
+// DirectMulti ...
+func DirectMulti(c *cli.Context, singlewords, multiwords []string) {
+	keywords := append(singlewords, multiwords...)
+
+	if c.NArg() == 0 {
+		for _, t := range keywords {
+			fmt.Println(t)
+		}
+		return
+	}
+
+	skip := 0
+	match := make(map[string]bool)
+
+	for _, t := range append([]string{c.Args().First()}, c.Args().Tail()...) {
+		if skip > 0 {
+			skip--
+			continue
+		}
+		skip = 1
+		match[t] = true
+		continue
+	}
+	// do not complete in positions where arguments are expected
+	if skip > 0 {
+		return
+	}
+	for _, t := range singlewords {
+		if !match[t] {
+			fmt.Println(t)
+		}
+	}
+	for _, t := range multiwords {
+		fmt.Println(t)
+	}
+}
+
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
