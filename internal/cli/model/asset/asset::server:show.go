51377793c3f404e944f4339e0de30a4d66d0eda8
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2021-10-07 13:52:36 +0200

    asset: cli support for asset::server:show

--- internal/cli/model/asset/asset::server:show.go
+++ internal/cli/model/asset/asset::server:show.go
@@ -8,10 +8,7 @@
 package asset // import "github.com/mjolnir42/tom/internal/cli/model/asset"
 
 import (
-	//	"fmt"
-	//	"strings"
-
-	//	"github.com/mjolnir42/tom/internal/cli/adm"
+	"github.com/mjolnir42/tom/internal/cli/adm"
 	"github.com/mjolnir42/tom/pkg/proto"
 	"github.com/urfave/cli/v2"
 )
@@ -21,7 +18,39 @@ func init() {
 }
 
 func cmdAssetServerShow(c *cli.Context) error {
-	return nil
+	opts := map[string][]string{}
+	if err := adm.VariadicArguments(
+		proto.CmdServerShow,
+		c,
+		&opts,
+	); err != nil {
+		return err
+	}
+
+	r := proto.Server{}
+	if _, ok := opts[`namespace`]; ok {
+		r.Namespace = opts[`namespace`][0]
+		r.Name = c.Args().First()
+		if err := proto.ValidNamespace(r.Namespace); err != nil {
+			return err
+		}
+		if err := proto.OnlyUnreserved(r.Name); err != nil {
+			return err
+		}
+	} else {
+		r.TomID = c.Args().First()
+		if err := r.ParseTomID(); err != nil {
+			return err
+		}
+	}
+
+	spec := adm.Specification{
+		Name: proto.CmdServerShow,
+		Placeholder: map[string]string{
+			proto.PlHoldTomID: r.FormatDNS(),
+		},
+	}
+	return adm.Perform(spec, c)
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
