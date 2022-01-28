dff6512c5334beab5df27001caea6cb993324a09
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-01-28 15:27:26 +0000

    asset: implement asset::orchestration:add

--- internal/model/asset/asset::orchestration:add.go
+++ internal/model/asset/asset::orchestration:add.go
@@ -8,11 +8,14 @@
 package asset // import "github.com/mjolnir42/tom/internal/model/asset/"
 
 import (
+	"database/sql"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/mjolnir42/tom/internal/msg"
 	"github.com/mjolnir42/tom/internal/rest"
+	"github.com/mjolnir42/tom/internal/stmt"
 	"github.com/mjolnir42/tom/pkg/proto"
 )
 
@@ -58,6 +61,143 @@ func (m *Model) OrchestrationAdd(w http.ResponseWriter, r *http.Request,
 	m.x.HM.MustLookup(&request).Intake() <- request
 	result := <-request.Reply
 	m.x.Send(&w, &result, exportOrchestrationAdd)
+}
+
+// add creates a new orchestration environment
+func (h *OrchestrationWriteHandler) add(q *msg.Request, mr *msg.Result) {
+	var (
+		res                            sql.Result
+		err                            error
+		tx                             *sql.Tx
+		txTime, validSince, validUntil time.Time
+		rows                           *sql.Rows
+		ok                             bool
+		orchID                         string
+	)
+	// setup a consistent transaction time timestamp that is used for all
+	// records
+	txTime = time.Now().UTC()
+
+	if err = msg.ResolveValidSince(
+		q.Orchestration.Property[`name`].ValidSince,
+		&validSince, &txTime,
+	); err != nil {
+		mr.BadRequest(err)
+		return
+	}
+
+	if err = msg.ResolveValidUntil(
+		q.Orchestration.Property[`name`].ValidUntil,
+		&validUntil, &txTime,
+	); err != nil {
+		mr.BadRequest(err)
+		return
+	}
+
+	// open transaction
+	if tx, err = h.conn.Begin(); err != nil {
+		mr.ServerError(err)
+		return
+	}
+
+	// discover all attributes and record them with their type
+	attrMap := map[string]string{}
+	if rows, err = tx.Query(
+		stmt.NamespaceAttributeDiscover,
+		q.Orchestration.Namespace,
+	); err != nil {
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	}
+	for rows.Next() {
+		var attribute, typ string
+		if err = rows.Scan(
+			&attribute,
+			&typ,
+		); err != nil {
+			rows.Close()
+			mr.ServerError(err)
+			tx.Rollback()
+			return
+		}
+		attrMap[attribute] = typ
+	}
+	if err = rows.Err(); err != nil {
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	}
+
+	// for all properties specified in the request, check that the attribute
+	// exists and create missing attributes
+	for key := range q.Orchestration.Property {
+		if _, ok = attrMap[key]; !ok {
+			if res, err = tx.Exec(
+				stmt.NamespaceAttributeAddStandard,
+				q.Orchestration.Namespace,
+				key,
+				q.UserIDLib,
+				q.AuthUser,
+			); err != nil {
+				mr.ServerError(err)
+				tx.Rollback()
+				return
+			}
+			if !mr.CheckRowsAffected(res.RowsAffected()) {
+				tx.Rollback()
+				return
+			}
+		}
+	}
+
+	// create named orchestration environment in specified namespace,
+	// this is an INSERT statement with a RETURNING clause, thus
+	// requires .QueryRow instead of .Exec
+	if err = tx.Stmt(
+		h.stmtAdd,
+	).QueryRow(
+		q.Orchestration.Namespace,
+		q.UserIDLib,
+		q.AuthUser,
+		q.Orchestration.Property[`name`].Value,
+		validSince,
+		validUntil,
+	).Scan(
+		&orchID,
+	); err == sql.ErrNoRows {
+		// query did not return the generated rteID
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	} else if err != nil {
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	}
+
+	// for all properties specified in the request, update the value.
+	// this transparently creates missing entries.
+	for key := range q.Orchestration.Property {
+		switch key {
+		case `name`:
+			continue
+		default:
+			if ok = h.txPropUpdate(
+				q, mr, tx, &txTime, q.Orchestration.Property[key], orchID,
+			); !ok {
+				tx.Rollback()
+				return
+			}
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		mr.ServerError(err)
+		return
+	}
+	mr.Orchestration = append(mr.Orchestration, q.Orchestration)
+	mr.OK()
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
