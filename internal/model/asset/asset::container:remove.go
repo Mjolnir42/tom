384611645d6b007d987ed7bae2bc1a2e31c5cc58
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-02-16 13:34:24 +0000

    asset: implement asset::container:remove

--- internal/model/asset/asset::container:remove.go
+++ internal/model/asset/asset::container:remove.go
@@ -8,7 +8,10 @@
 package asset // import "github.com/mjolnir42/tom/internal/model/asset/"
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/mjolnir42/tom/internal/msg"
@@ -40,9 +43,8 @@ func (m *Model) ContainerRemove(w http.ResponseWriter, r *http.Request,
 	request := msg.New(r, params)
 	request.Section = msg.SectionContainer
 	request.Action = proto.ActionRemove
-	request.Container = proto.Container{
-		TomID: params.ByName(`tomID`),
-	}
+	request.Container = *(proto.NewContainer())
+	request.Container.TomID = params.ByName(`tomID`)
 
 	if err := request.Container.ParseTomID(); err != nil {
 		if err != proto.ErrEmptyTomID {
@@ -63,8 +65,172 @@ func (m *Model) ContainerRemove(w http.ResponseWriter, r *http.Request,
 
 // remove ...
 func (h *ContainerWriteHandler) remove(q *msg.Request, mr *msg.Result) {
-	// TODO
-	mr.NotImplemented()
+	var (
+		txTime                                           time.Time
+		containerID, dictionaryID, createdAt, createdBy  string
+		nameValidSince, nameValidUntil, namedAt, namedBy string
+		err                                              error
+		rows                                             *sql.Rows
+		tx                                               *sql.Tx
+		ok, done                                         bool
+	)
+	// setup a consistent transaction time timestamp that is used for all
+	// records
+	txTime = time.Now().UTC()
+
+	// open transaction
+	if tx, err = h.conn.Begin(); err != nil {
+		mr.ServerError(err)
+		return
+	}
+
+	// discover containerID at the start of the transaction
+	if err = tx.Stmt(
+		h.stmtTxShow,
+	).QueryRow(
+		q.Container.Namespace,
+		q.Container.Name,
+		txTime,
+	).Scan(
+		&containerID,
+		&dictionaryID,
+		&createdAt,
+		&createdBy,
+		&nameValidSince,
+		&nameValidUntil,
+		&namedAt,
+		&namedBy,
+	); err == sql.ErrNoRows {
+		mr.NotFound(err)
+		tx.Rollback()
+		return
+	} else if err != nil {
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	}
+
+	// unstack the container itself
+	if _, err = tx.Stmt(
+		h.stmtTxStackClamp,
+	).Exec(
+		txTime,
+		containerID,
+	); err != nil {
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	}
+
+	// discover all attributes and record them with their type
+	attrMap := map[string]string{}
+	if rows, err = tx.Stmt(
+		h.stmtAttQueryDiscover,
+	).Query(
+		q.Container.Namespace,
+	); err != nil {
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	}
+	for rows.Next() {
+		var attribute, typ string
+		if err = rows.Scan(
+			&attribute,
+			&typ,
+		); err != nil {
+			rows.Close()
+			mr.ServerError(err)
+			tx.Rollback()
+			return
+		}
+		attrMap[attribute] = typ
+	}
+	if err = rows.Err(); err != nil {
+		mr.ServerError(err)
+		tx.Rollback()
+		return
+	}
+
+	// for all properties, any currently valid value must
+	// be invalided by setting its validUntil value to the time of the
+	// transaction
+	for key := range attrMap {
+		var stmtSelect, stmtClamp *sql.Stmt
+
+		switch attrMap[key] {
+		case proto.AttributeStandard:
+			stmtSelect = tx.Stmt(h.stmtTxStdPropSelect)
+			stmtClamp = tx.Stmt(h.stmtTxStdPropClamp)
+		case proto.AttributeUnique:
+			stmtSelect = tx.Stmt(h.stmtTxUniqPropSelect)
+			stmtClamp = tx.Stmt(h.stmtTxUniqPropClamp)
+		default:
+			mr.ServerError()
+			tx.Rollback()
+			return
+		}
+
+		if ok, done = h.txPropClamp(
+			q, mr, tx, &txTime, stmtSelect, stmtClamp,
+			proto.PropertyDetail{
+				Attribute: key,
+				// construct an imaginary new value for the property. The clamping
+				// function does not invalidate the current value, if the provided
+				// imaginary record matches the existing record in both value and
+				// upper validUntil bound
+				Value: txTime.Format(msg.RFC3339Milli) + key + `_clamp`,
+			},
+			// send the transaction time as the imaginary new value's requested
+			// validSince:
+			// - guaranteed to be after the current value's validSince, as it
+			//   otherwise would not be valid
+			// - this value is used as the new validUntil boundary of the exitsing
+			//   value
+			txTime,
+			// send the transaction time as the imaginary new value's requested
+			// validUntil. This is matched against the existing value's validUntil
+			// to determine if the record needs clamping
+			txTime,
+			// the ID of the container being edited
+			containerID,
+		); !ok {
+			// appropriate error function is already called by h.txPropClamp
+			tx.Rollback()
+			return
+		} else if done {
+			// txPropClamp did nothing because the existing record matched both
+			// time and value. Retry with a different value.
+			if ok, done = h.txPropClamp(
+				q, mr, tx, &txTime, stmtSelect, stmtClamp,
+				proto.PropertyDetail{
+					Attribute: key,
+					Value:     txTime.Format(msg.RFC3339Milli) + key + `_alternate`,
+				},
+				txTime,
+				txTime,
+				containerID,
+			); !ok {
+				tx.Rollback()
+				return
+			} else if done {
+				// this should not be possible -> abort.
+				mr.ServerError(fmt.Errorf(
+					"txPropClamp encountered impossible repeat matches for %s",
+					key,
+				))
+				tx.Rollback()
+				return
+			}
+		}
+	}
+
+	if err = tx.Commit(); err != nil {
+		mr.ServerError(err)
+		return
+	}
+	mr.Container = append(mr.Container, q.Container)
+	mr.OK()
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
