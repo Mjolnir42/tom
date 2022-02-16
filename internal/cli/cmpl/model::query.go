6b11fb5d3cb7743fe007323a8fd98f35afa56c76
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-02-16 09:18:50 +0000

    cli: add query command

---
+++ internal/cli/cmpl/model::query.go
@@ -0,0 +1,18 @@
+/*-
+ * Copyright (c) 2022, Jörg Pernfuß <joerg.pernfuss@ionos.com>
+ * All rights reserved
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package cmpl
+
+import (
+	"github.com/urfave/cli/v2"
+)
+
+func QueryResolver(c *cli.Context) {
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
