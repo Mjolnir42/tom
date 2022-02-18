6b12bf73d60e3040cf7bbc6e36b2ba955240e70c
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-02-18 08:50:00 +0000

    meta: allow referential namespaces to not have a lookup-uri
    It would of course be better if every system has an API, but
    sometimes things don't. Allow for this by updating the requirement
    to a SHOULD (instead of MUST).
    
    For this, update the SelectResource SQL statements to eliminate
    the resultset via join when there is no uri result.

--- internal/stmt/asset::server::read.go
+++ internal/stmt/asset::server::read.go
@@ -315,7 +315,8 @@ WITH dict AS ( SELECT meta.dictionary.dictionaryID
                 AND   meta.standard_attribute.attributeID = meta.dictionary_standard_attribute_values.attributeID
                WHERE  meta.standard_attribute.attribute = 'dict_lookup'
                  AND  $3::timestamptz(3) <@ meta.dictionary_standard_attribute_values.validity),
-      uri AS ( SELECT meta.dictionary_standard_attribute_values.value AS uri
+      uri AS ( SELECT meta.dictionary_standard_attribute_values.value AS uri,
+                      meta.dictionary.dictionaryID
                FROM   meta.dictionary
                JOIN   dict
                  ON   dict.dictionaryID = meta.dictionary.dictionaryID
@@ -338,7 +339,8 @@ JOIN                  asset.server_unique_attribute_values
  AND                  asset.server.serverID = asset.server_unique_attribute_values.serverID
 JOIN                  look
   ON                  meta.unique_attribute.attribute = look.key
-CROSS JOIN            uri
+JOIN                  uri
+  ON                  asset.server.dictionaryID = uri.dictionaryID
 WHERE                 asset.server.serverID = $2::uuid
   AND                 $3::timestamptz(3) <@ asset.server_unique_attribute_values.validity;`
 )
