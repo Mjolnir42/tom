1e6a4ddf27b8a5d5a46532771633eedf17d984db
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2022-11-04 11:05:43 +0100

    cred: move client credential handling to separate package

---
+++ internal/cred/create_passphrase_file.go
@@ -0,0 +1,33 @@
+/*-
+ * Copyright (c) 2022, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package cred // import "github.com/mjolnir42/tom/internal/cred"
+
+import (
+	cryptorand "crypto/rand"
+	"fmt"
+	"io"
+	"os"
+)
+
+func createPassphraseFile(path string, phrase *string) error {
+	d := make([]byte, 32)
+	if _, err := io.ReadFull(cryptorand.Reader, d[:32]); err != nil {
+		return err
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(f, "%x\n", d)
+	f.Close()
+	*phrase = string(mask([]byte(fmt.Sprintf("%x", d))))
+	return nil
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
---
+++ internal/cred/register.go
@@ -0,0 +1,66 @@
+/*-
+ * Copyright (c) 2022, Jörg Pernfuß
+ *
+ * Use of this source code is governed by a 2-clause BSD license
+ * that can be found in the LICENSE file.
+ */
+
+package cred // import "github.com/mjolnir42/tom/internal/cred"
+
+import (
+	"encoding/base64"
+	"flag"
+	"os"
+	"time"
+
+	"github.com/Showmax/go-fqdn"
+	"github.com/mjolnir42/epk"
+	"github.com/mjolnir42/tom/internal/cli/adm"
+	"github.com/mjolnir42/tom/pkg/proto"
+	"github.com/urfave/cli/v2"
+	"golang.org/x/crypto/ed25519"
+)
+
+func registerMachineEnrollment(pub *ed25519.PublicKey, priv *epk.EncryptedPrivateKey, phrase string, ctx *cli.Context) error {
+	var err error
+	var sig []byte
+
+	req := proto.NewUserRequest()
+	req.User.LibraryName = `engineroom`
+	if req.User.UserName, err = GetHash(pub); err != nil {
+		return err
+	}
+	if req.User.FirstName, err = os.Hostname(); err != nil {
+		return err
+	}
+	if req.User.LastName, err = fqdn.FqdnHostname(); err != nil {
+		return err
+	}
+	req.User.Credential.Category = proto.CredentialPubKey
+	req.User.Credential.Value = base64.StdEncoding.EncodeToString(*pub)
+
+	req.Auth.Timestamp = time.Now().Format(time.RFC3339)
+	req.Auth.UserID = req.User.UserName
+	if err = req.CalculateDataHash(); err != nil {
+		return err
+	}
+	if sig, err = priv.Sign(phrase, []byte(req.Auth.Sig.DataHash)); err != nil {
+		return err
+	}
+	req.Auth.Sig.Signature = string(sig)
+
+	spec := adm.Specification{
+		Name: proto.CmdMachEnrol,
+		Placeholder: map[string]string{
+			proto.PlHoldTomID: req.User.UserName,
+		},
+		Body: req,
+	}
+	if ctx == nil {
+		ctx = cli.NewContext(cli.NewApp(), flag.NewFlagSet(``, flag.ContinueOnError), nil)
+	}
+
+	return adm.Perform(spec, ctx)
+}
+
+// vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
