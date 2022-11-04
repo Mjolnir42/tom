acba1d7182cc340884e45011585c57849436a9e3
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2022-11-04 15:46:17 +0100

    iam: update machine enrolment

--- internal/model/iam/iam::machine:enrol.go
+++ internal/model/iam/iam::machine:enrol.go
@@ -9,7 +9,9 @@ package iam // import "github.com/mjolnir42/tom/internal/model/iam"
 
 import (
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"hash"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -17,6 +19,7 @@ import (
 	"github.com/mjolnir42/tom/internal/rest"
 	"github.com/mjolnir42/tom/internal/stmt"
 	"github.com/mjolnir42/tom/pkg/proto"
+	"golang.org/x/crypto/blake2b"
 )
 
 func init() {
@@ -48,7 +51,7 @@ func (m *Model) MachineEnrol(w http.ResponseWriter, r *http.Request,
 		proto.ActionEnrolment,
 	)
 
-	req := proto.Request{}
+	req := proto.NewUserRequest()
 	if err := rest.DecodeJSONBody(r, &req); err != nil {
 		m.x.ReplyBadRequest(&w, &request, err)
 		return
@@ -75,9 +78,25 @@ func (m *Model) MachineEnrol(w http.ResponseWriter, r *http.Request,
 		return
 	}
 
-	if !m.x.IsAuthorized(&request) {
-		m.x.ReplyForbidden(&w, &request)
-		return
+	switch request.User.Credential.Category {
+	case proto.CredentialPubKey:
+		if ok, err := req.Verify(); err != nil {
+			m.x.ReplyBadRequest(&w, &request, err)
+			return
+		} else if !ok {
+			m.x.LM.GetLogger(`application`).Infoln(
+				request.ID, `Request signature verification failed`,
+				req.Auth.Sig.DataHash,
+				req.Auth.Sig.Signature,
+			)
+			m.x.ReplyForbidden(&w, &request)
+			return
+		}
+	default:
+		if !m.x.IsAuthorized(&request) {
+			m.x.ReplyForbidden(&w, &request)
+			return
+		}
 	}
 
 	m.x.HM.MustLookup(&request).Intake() <- request
@@ -93,6 +112,8 @@ func (h *UserWriteHandler) enrolment(q *msg.Request, mr *msg.Result) {
 		enrolmentKey               sql.NullString
 		tx                         *sql.Tx
 		res                        sql.Result
+		hfunc                      hash.Hash
+		key, dgst                  []byte
 	)
 
 	if tx, err = h.conn.Begin(); err != nil {
@@ -145,6 +166,7 @@ func (h *UserWriteHandler) enrolment(q *msg.Request, mr *msg.Result) {
 		q.User.FirstName,
 		q.User.LastName,
 		q.User.UserName,
+		`ffffffff-ffff-ffff-ffff-ffffffffffff`,
 	).Scan(
 		&q.User.ID,
 	); err == sql.ErrNoRows {
@@ -166,10 +188,48 @@ func (h *UserWriteHandler) enrolment(q *msg.Request, mr *msg.Result) {
 		return
 	}
 
+	switch q.User.Credential.Category {
+	case proto.CredentialPubKey:
+		// calculate key fingerprint
+		if hfunc, err = blake2b.New(16, []byte(`engineroom.machine.tom`)); err != nil {
+			mr.ServerError(err)
+			return
+		}
+		if key, err = base64.StdEncoding.DecodeString(q.User.Credential.Value); err != nil {
+			mr.ServerError(err)
+			return
+		}
+		hfunc.Write(key)
+		dgst = hfunc.Sum(nil)
+
+		// add key credential
+		if res, err = tx.Stmt(h.stmtAddKey).Exec(
+			q.User.ID,
+			q.User.Credential.Value,
+			fmt.Sprintf("%x", dgst),
+			q.User.UserName,
+			q.User.LibraryName,
+			q.User.ID,
+		); err != nil {
+			mr.ServerError(err)
+			return
+		}
+		if !mr.AssertOneRowAffected(res.RowsAffected()) {
+			return
+		}
+
+	default:
+		mr.ExpectationFailed(
+			fmt.Errorf(`Machine registration should contain credential of type public-key`),
+		)
+	}
+
 	if err = tx.Commit(); err != nil {
 		mr.ServerError(err)
 		return
 	}
+	q.User.IsActive = true
+	q.User.Credential = nil
 	mr.User = append(mr.User, q.User)
 	mr.OK()
 }
