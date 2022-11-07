22c8b5ac32f89be376d143d4d0b0ee0bb5fd0259
Pernfuß, Jörg <joerg.pernfuss@ionos.com>
2022-11-07 15:45:16 +0100

    stmt: add UserKey statement

--- internal/stmt/iam::user::read.go
+++ internal/stmt/iam::user::read.go
@@ -15,11 +15,26 @@ SELECT      'TODO::UserList'::text;`  // TODO
 
 	UserShow = `
 SELECT      'TODO::UserShow'::text;`  // TODO
+
+	UserKey = `
+SELECT      publickey,
+            fingerprint
+FROM        inventory.user_key
+JOIN        inventory.user
+  ON        inventory.user_key.userID = inventory.user.userID
+JOIN        inventory.identity_library
+  ON        inventory.user.identityLibraryID = inventory.identity_library.identityLibraryID
+WHERE       now()::timestamptz(3) <@ inventory.user_key.validity
+  AND       inventory.user.uID = $1::text
+  AND       inventory.identity_library.name = $2::text
+  AND       inventory.user.isActive
+  AND NOT   inventory.user.isDeleted;`
 )
 
 func init() {
 	m[UserList] = `UserList`
 	m[UserShow] = `UserShow`
+	m[UserKey] = `UserKey`
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
