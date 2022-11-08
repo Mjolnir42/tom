fc64385b29ac051e2f2c01db1844236ecd112e3d
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-11-08 18:26:28 +0000

    ipfix: wip savepoint procFilter

--- internal/ipfix/filter.go
+++ internal/ipfix/filter.go
@@ -16,6 +16,9 @@ import (
 
 type procFilter struct {
 	conf    config.SettingsIPFIX
+	quit    chan interface{}
+	exit    chan interface{}
+	err     chan error
 	inpipe  chan []byte
 	outpipe chan []byte
 	mirror  chan []byte
@@ -26,6 +29,9 @@ type procFilter struct {
 func newFilter(conf config.SettingsIPFIX, inpipe, outpipe, mirror chan []byte, pool *sync.Pool, lm *lhm.LogHandleMap) (*procFilter, error) {
 	f := &procFilter{
 		conf:    conf,
+		quit:    make(chan interface{}),
+		exit:    make(chan interface{}),
+		err:     make(chan error),
 		inpipe:  inpipe,
 		outpipe: outpipe,
 		mirror:  mirror,
@@ -39,6 +45,73 @@ func newFilter(conf config.SettingsIPFIX, inpipe, outpipe, mirror chan []byte, p
 	// write to outpipe
 
 	return f, nil
+}
+
+func (f *procFilter) serve() {
+	defer f.wg.Done()
+	f.lm.GetLogger(`application`).Infoln(`IPFIX|tlsServer: start serving clients`)
+
+	connections := make(chan net.Conn)
+
+	f.wg.Add(1)
+	go func() {
+		defer f.wg.Done()
+
+	acceptloop:
+		for {
+			conn, err := f.listener.Accept()
+			if err != nil {
+				select {
+				case <-f.quit:
+					break acceptloop
+				default:
+					f.err <- fmt.Errorf("tlsServer/Accept/fatal: %w", err)
+					close(f.exit)
+					break acceptloop
+				}
+			}
+			connections <- conn
+		}
+	}()
+
+serveloop:
+	for {
+		select {
+		case conn := <-connections:
+			f.wg.Add(1)
+			go func() {
+				f.lm.GetLogger(`application`).Printf("IPFIX|tlsServer: accepted connection from: %s\n",
+					conn.RemoteAddr().String(),
+				)
+				f.handleConnection(conn)
+				f.wg.Done()
+			}()
+		case <-f.exit:
+			f.lm.GetLogger(`application`).Println(`IPFIX|tlsServer: goroutine indicated fatal error`)
+			break serveloop
+		case <-f.quit:
+			f.lm.GetLogger(`application`).Println(`IPFIX|tlsServer: received shutdown signal`)
+			break serveloop
+		}
+	}
+}
+
+func (f *procFilter) Err() chan error {
+	return f.err
+}
+
+func (f *procFilter) Exit() chan interface{} {
+	return f.exit
+}
+
+func (f *procFilter) Stop() chan error {
+	go func(e chan error) {
+		close(f.quit)
+		f.listener.Close()
+		f.wg.Wait()
+		close(e)
+	}(f.Err())
+	return f.Err()
 }
 
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
