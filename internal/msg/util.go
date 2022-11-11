71b524bbb0410218ad4e8baa60c17117962603ac
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-11-11 10:47:21 +0000

    msg: drop unused functions ParseValidSince,ParseValidUntil

--- internal/msg/util.go
+++ internal/msg/util.go
@@ -84,48 +84,4 @@ func ResolvePValidUntil(s string, t, tx *time.Time) (err error) {
 	return
 }
 
-func ParseValidSince(s string, txNow *time.Time) (since *time.Time, err error) {
-	// no consistent transaction timestamp for now was supplied
-	if txNow == nil {
-		*(txNow) = time.Now().UTC()
-	}
-
-	switch s {
-	case `always`, `perpetual`:
-		*(since) = NegTimeInf
-	case `forever`:
-		err = ErrInvalidValidity
-	case ``, `now`:
-		since = txNow
-	default:
-		*(since), err = time.Parse(
-			RFC3339Milli,
-			s,
-		)
-	}
-	return since, err
-}
-
-func ParseValidUntil(s string, txNow *time.Time) (until *time.Time, err error) {
-	// no consistent transaction timestamp for now was supplied
-	if txNow == nil {
-		*(txNow) = time.Now().UTC()
-	}
-
-	switch s {
-	case `always`:
-		err = ErrInvalidValidity
-	case ``, `forever`, `perpetual`:
-		*(until) = PosTimeInf
-	case `now`:
-		until = txNow
-	default:
-		*(until), err = time.Parse(
-			RFC3339Milli,
-			s,
-		)
-	}
-	return until, err
-}
-
 // vim: ts=4 sw=4 sts=4 noet fenc=utf-8 ffs=unix
