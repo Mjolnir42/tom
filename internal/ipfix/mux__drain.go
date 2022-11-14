ed390a6fdd51c2a0954d9366701cc6f3f8d13998
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-11-14 10:17:47 +0000

    mux: drain inactive outputs

--- internal/ipfix/mux__drain.go
+++ internal/ipfix/mux__drain.go
@@ -8,6 +8,7 @@
 package ipfix
 
 import (
+	"fmt"
 )
 
 func (m *ipfixMux) drainDiscardChannels() {
@@ -27,6 +28,116 @@ drainloop:
 		case <-m.discardFDR:
 			m.lm.GetLogger(`error`).
 				Errorln(`ipfix.mux: drained stray message from channel discardFDR`)
+		}
+	}
+}
+
+// opportunUDP is an opportunistic drain for channel outUDP that reads
+// from the channel if no reader is configured.
+func (m *ipfixMux) opportunUDP() {
+	defer m.wg.Done()
+	if m.fOutUDP {
+		// udpClient is active
+		return
+	}
+
+drainloop:
+	for {
+		select {
+		case <-m.quit:
+			break drainloop
+		case <-m.outUDP:
+			m.err <- fmt.Errorf(
+				"ipfix.mux: drained stray message from channel %s", `outUDP`,
+			)
+		}
+	}
+}
+
+// opportunTCP is an opportunistic drain for channel outTCP that reads
+// from the channel if no reader is configured.
+func (m *ipfixMux) opportunTCP() {
+	defer m.wg.Done()
+	if m.fOutTCP {
+		// tcpClient is active
+		return
+	}
+
+drainloop:
+	for {
+		select {
+		case <-m.quit:
+			break drainloop
+		case <-m.outTCP:
+			m.err <- fmt.Errorf(
+				"ipfix.mux: drained stray message from channel %s", `outTCP`,
+			)
+		}
+	}
+}
+
+// opportunTLS is an opportunistic drain for channel outTLS that reads
+// from the channel if no reader is configured.
+func (m *ipfixMux) opportunTLS() {
+	defer m.wg.Done()
+	if m.fOutTLS {
+		// tlsClient is active
+		return
+	}
+
+drainloop:
+	for {
+		select {
+		case <-m.quit:
+			break drainloop
+		case <-m.outTLS:
+			m.err <- fmt.Errorf(
+				"ipfix.mux: drained stray message from channel %s", `outTLS`,
+			)
+		}
+	}
+}
+
+// opportunJSON is an opportunistic drain for channel outJSON that reads
+// from the channel if no reader is configured.
+func (m *ipfixMux) opportunJSON() {
+	defer m.wg.Done()
+	if m.fOutJSN {
+		// jsonClient is active
+		return
+	}
+
+drainloop:
+	for {
+		select {
+		case <-m.quit:
+			break drainloop
+		case <-m.outJSN:
+			m.err <- fmt.Errorf(
+				"ipfix.mux: drained stray message from channel %s", `outJSN`,
+			)
+		}
+	}
+}
+
+// opportunAGG is an opportunistic drain for channel outAGG that reads
+// from the channel if no reader is configured.
+func (m *ipfixMux) opportunAGG() {
+	defer m.wg.Done()
+	if m.aggregation {
+		// aggregation is active
+		return
+	}
+
+drainloop:
+	for {
+		select {
+		case <-m.quit:
+			break drainloop
+		case <-m.outAGG:
+			m.err <- fmt.Errorf(
+				"ipfix.mux: drained stray message from channel %s", `outAGG`,
+			)
 		}
 	}
 }
