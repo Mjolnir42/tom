524c8ad8ef4a693a3d462256447ee44d91aed20d
Jörg Pernfuß <joerg.pernfuss@ionos.com>
2022-11-23 21:32:27 +0000

    ipfix: store flow aggregation information

--- internal/ipfix/aggregate.go
+++ internal/ipfix/aggregate.go
@@ -25,6 +25,7 @@ type procAggregate struct {
 	quit chan interface{}
 	exit chan interface{}
 	err  chan error
+	agg  map[string]*Conn
 }
 
 func newAggregate(conf config.SettingsIPFIX, mux *ipfixMux, pool *sync.Pool, lm *lhm.LogHandleMap) (*procAggregate, error) {
@@ -54,18 +55,20 @@ runloop:
 		case <-m.quit:
 			m.lm.GetLogger(`application`).Infoln(`ipfix.aggregation: shutdown signal received`)
 			break runloop
-		case <-m.pipe:
+		case r := <-m.pipe:
+
+			srcAddress := net.ParseIP(r.SrcAddress)
+			dstAddress := net.ParseIP(r.DstAddress)
+
+			c := &Conn{
+				Protocol: r.Protocol,
+			}
+
 			// XXX TODO process record
 			//
-			// flowset-collection-name => hostname / type slamdd-go/ipfix
-			// 1. normalize ordering src,dst
 			// 2. check for local listening socket (https://github.com/cakturk/go-netstat)
 			//    => if match, set remote port 0
 			//    => if not match, set local port 0
-			// 3. concat(srcaddress,dstaddress,srcport,dstport,proto) => asByte =>  hex-encode
-			//    hexcode_json => attribute property
-			// 4. set property validUntil +24h
-			// 5. send proto.CmdFlowEnsure request to Tom
 			srcPort, dstPort := ephemeralPorts(r.SrcPort, r.DstPort)
 
 			// srcAddress a, dstAddress b
@@ -89,16 +92,153 @@ runloop:
 				c.DstAddress = srcAddress.String()
 				c.DstPort = srcPort
 			}
+
+			if _, ok := m.agg[c.String()]; !ok {
+				m.agg[c.String()] = c
+			}
+			m.agg[c.String()].Seen()
 		}
 	}
 }
 
 type Conn struct {
-	SrcAddress string `json:"srcAddr"`
-	SrcPort    int    `json:"srcPort"`
-	DstAddress string `json:"dstAddr"`
-	DstPort    int    `json:"dstPort"`
-	Protocol   string `json:"proto"`
+	SrcAddress string    `json:"srcAddr"`
+	SrcPort    int       `json:"srcPort"`
+	DstAddress string    `json:"dstAddr"`
+	DstPort    int       `json:"dstPort"`
+	Protocol   string    `json:"proto"`
+	LastSeen   time.Time `json:"-"`
+	CacheUntil time.Time `json:"-"`
+	hash       string    `json:"-"`
+}
+
+func (c *Conn) String() string {
+	if c.hash != `` {
+		return c.hash
+	}
+
+	h := fnv.New128a()
+	h.Write([]byte(c.SrcAddress))
+	h.Write([]byte(strconv.Itoa(c.SrcPort)))
+	h.Write([]byte(c.DstAddress))
+	h.Write([]byte(strconv.Itoa(c.DstPort)))
+	h.Write([]byte(c.Protocol))
+	c.hash = hex.EncodeToString(h.Sum(nil))
+	return c.hash
+}
+
+func (c *Conn) Seen() {
+	c.LastSeen = time.Now().UTC()
+
+	if c.CacheUntil.IsZero() {
+		c.CacheUntil = c.LastSeen.Add(2 * twelveHours)
+		return
+	}
+
+	if c.LastSeen.Add(twelveHours).After(c.LastSeen) {
+		c.CacheUntil = c.CacheUntil.Add(twelveHours)
+	}
+}
+
+func (c *Conn) Prune(now *time.Time) bool {
+	if c.LastSeen.Add(twelveHours).Before(*now) {
+		return true
+	}
+	return false
+}
+
+func (m *procAggregate) cron() {
+	t := time.Now()
+	n := time.Date(t.Year(), t.Month(), t.Day(), 0, 0, 0, 0, t.Location())
+	d := n.Sub(t)
+	if d < 0 {
+		n = n.Add(24 * time.Hour)
+		d = n.Sub(t)
+	}
+	select {
+	case <-m.quit:
+		return
+	case <-m.exit:
+		return
+	case <-time.After(d):
+	}
+
+	tick := time.NewTicker(24 * time.Hour)
+	<-m.prune()
+	<-m.export()
+
+	for {
+		select {
+		case <-m.quit:
+			break
+		case <-m.exit:
+			break
+		case <-tick.C:
+			<-m.prune()
+			<-m.export()
+		}
+	}
+}
+
+func (m *procAggregate) prune() chan interface{} {
+	ret := make(chan interface{})
+	go func(x chan interface{}) {
+		m.mtx.Lock()
+		defer m.mtx.Unlock()
+		defer close(x)
+
+		// XXX TODO
+	}(ret)
+	return ret
+}
+
+func (m *procAggregate) export() chan interface{} {
+	ret := make(chan interface{})
+	go func(x chan interface{}) {
+		m.mtx.RLock()
+		defer m.mtx.RUnlock()
+		defer close(x)
+
+		h, err := os.Hostname()
+		if err != nil {
+			m.err <- err
+			return
+		}
+		h, _, _ = strings.Cut(h, `.`)
+
+		req := proto.NewFlowRequest()
+		req.Flow.Namespace = `fw-flow`
+		req.Flow.Name = h
+		req.Flow.Type = `slamdd-go/ipfix`
+
+		for k, v := range m.agg {
+			prop := proto.PropertyDetail{
+				Attribute:  k + `_json`,
+				ValidSince: v.LastSeen.Format(time.RFC3339),
+				ValidUntil: v.CacheUntil.Format(time.RFC3339),
+			}
+			b, err := json.Marshal(v)
+			if err != nil {
+				m.err <- err
+				continue
+			}
+			prop.Value = string(b)
+			req.Flow.Property[prop.Attribute] = prop
+		}
+
+		spec := adm.Specification{
+			Name: proto.CmdFlowEnsure,
+			Body: req,
+		}
+
+		ctx := cli.NewContext(cli.NewApp(), flag.NewFlagSet(``, flag.ContinueOnError), nil)
+
+		err = adm.Perform(spec, ctx)
+		if err != nil {
+			m.err <- err
+		}
+	}(ret)
+	return ret
 }
 
 func ephemeralPorts(src, dst uint16) (int, int) {
